04f525de24720ce8ea1ac2124f9fe49ef0c33c75
bapic <itsbapic@gmail.com>
2023-02-25 17:02:34 -0800

    Add Prettier Config

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,29 +1,26 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const [owner, otherAccount] = await ethers.getSigners();
+	const [owner, otherAccount] = await ethers.getSigners();
 
-  const transactionCount = await owner.getTransactionCount();
+	const transactionCount = await owner.getTransactionCount();
 
-  // gets the address of the token before it is deployed
-  const futureAddress = ethers.utils.getContractAddress({
-    from: owner.address,
-    nonce: transactionCount + 1,
-  });
+	// gets the address of the token before it is deployed
+	const futureAddress = ethers.utils.getContractAddress({
+		from: owner.address,
+		nonce: transactionCount + 1,
+	});
 
-  const MyGovernor = await ethers.getContractFactory("MyGovernor");
-  const governor = await MyGovernor.deploy(futureAddress);
+	const MyGovernor = await ethers.getContractFactory("MyGovernor");
+	const governor = await MyGovernor.deploy(futureAddress);
 
-  const MyToken = await ethers.getContractFactory("MyToken");
-  const token = await MyToken.deploy(governor.address, "thisISATOKEN", "LOL");
+	const MyToken = await ethers.getContractFactory("MyToken");
+	const token = await MyToken.deploy(governor.address, "thisISATOKEN", "LOL");
 
-  console.log(
-    `Governor deployed to ${governor.address}`,
-    `Token deployed to ${token.address}`
-  );
+	console.log(`Governor deployed to ${governor.address}`, `Token deployed to ${token.address}`);
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
