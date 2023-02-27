11bda971aa02066856ab05f4010c4bf48bbd3bcd
Namdar12 <namdar.mesri12@gmail.com>
2023-02-27 08:56:53 -0800

    Fix backend deploy script type errors

--- backend/scripts/deploy.ts
+++ backend/scripts/deploy.ts
@@ -1,19 +1,33 @@
 import { ethers } from 'hardhat'
 
 async function main() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [owner, otherAccount] = await ethers.getSigners()
+  const [owner] = await ethers.getSigners()
 
-  const transactionCount: number = await owner.getTransactionCount()
+  const transactionCount = await owner.getTransactionCount()
 
   // gets the address of the token before it is deployed
-  const futureAddress = ethers.utils.getContractAddress({
+  const futureTokenAddress = ethers.utils.getContractAddress({
     from: owner.address,
     nonce: transactionCount + 1
   })
+  const futureTimelockAddress = ethers.utils.getContractAddress({
+    from: owner.address,
+    nonce: transactionCount + 2
+  })
 
-  const MyGovernor = await ethers.getContractFactory('MyGovernor')
-  const governor = await MyGovernor.deploy(futureAddress)
+  // Voting period is in blocks
+  const votingPeriod = 1000
+  // Fraction is in percentages (10 means 10%, etc)
+  const quorumFraction = 50
+
+  const MyGovernor = await ethers.getContractFactory('GovernorContractTimeLock')
+  const governor = await MyGovernor.deploy(
+    'Lil Dao Wow',
+    futureTokenAddress,
+    futureTimelockAddress,
+    votingPeriod,
+    quorumFraction
+  )
 
   const MyToken = await ethers.getContractFactory('MyToken')
   const token = await MyToken.deploy(governor.address, 'thisISATOKEN', 'LOL')
