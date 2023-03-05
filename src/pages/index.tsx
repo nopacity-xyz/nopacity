f8e26e0b36a07904e7dff4924a759fe64f4d6bf8
bapic <itsbapic@gmail.com>
2023-03-05 03:38:43 -0800

    Vercel Redeployment for SSL Certificate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <Layout>
       <Head>
         <title>Nopacity</title>
-        <meta name="description" content="Create a Group in 2 clicks." />
+        <meta name="description" content="Create a Group in 2 clicks. " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
