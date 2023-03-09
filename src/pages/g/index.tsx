22fa09edde6c7eb8eb40467dc65b99d5298d580f
Samuel Holmes <sam.lb.holmes@gmail.com>
2023-03-09 00:23:15 -0800

    Update group list page to use a table

--- src/pages/g/index.tsx
+++ src/pages/g/index.tsx
@@ -1,16 +1,17 @@
 import {
   Box,
   Button,
-  Card,
-  CardBody,
-  CardFooter,
   Center,
   Flex,
   Heading,
-  // Image,
   Link,
-  Stack,
-  Text
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr
 } from '@chakra-ui/react'
 import { ethers } from 'ethers'
 import React, { useEffect, useState } from 'react'
@@ -81,7 +82,7 @@ export default function Groups(props: GetGroupProps) {
           shadow="0px 0px 5px rgba(255,255,255,0.3), 1px 1px 5px rgba(255,255,255,0.3),10px 10px 30px rgba(0,0,0,0.3)"
         >
           <Center>
-            <Box>
+            <Box flexGrow={1}>
               <Flex>
                 <Heading
                   w="100%"
@@ -94,41 +95,45 @@ export default function Groups(props: GetGroupProps) {
                   Groups
                 </Heading>
               </Flex>
-              <Flex flexDirection="column">
-                {loading ? 'Loading...' : null}
-                {daoData.map(data => {
-                  return (
-                    <SingleGroupItem
-                      key={data.address}
-                      groupAddress={data.address}
-                      groupName={data.name}
-                      groupDescription={data.description}
-                      // numMembers={40}
-                    />
-                  )
-                })}
-                {/* <SingleGroupItem
-                      groupAddress="0x77a11df57295e5d6d4923872223a75bd96887a3a"
-                      groupName="Test Group Name"
-                      groupDescription="Lorem ipsum mofo. Very intesting description describing the purpose of the group!"
-                      numMembers={40}
-                    />
-                  </Flex>
-                  <Flex>
-                    <SingleGroupItem
-                      groupAddress="0x77a11df57295e5d6d4923872223a75bd96887a3a"
-                      groupName="Test Group Name"
-                      groupDescription="Lorem ipsum mofo. Very intesting description describing the purpose of the group!"
-                      numMembers={40}
-                    />
-                    <SingleGroupItem
-                      groupAddress="0x77a11df57295e5d6d4923872223a75bd96887a3a"
-                      groupName="Test Group Name"
-                      groupDescription="Lorem ipsum mofo. Very intesting description describing the purpose of the group!"
-                      numMembers={40}
-                    />
-                  */}
-              </Flex>
+              {loading ? 'Loading...' : null}
+              <TableContainer>
+                <Table variant="simple">
+                  <Thead>
+                    <Tr>
+                      <Th>Name</Th>
+                      <Th>Description</Th>
+                      <Th />
+                      {/* <Th isNumeric>Members</Th> */}
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {daoData.map(data => {
+                      return (
+                        <Tr key={data.address}>
+                          <Td>{data.name}</Td>
+                          <Td>{data.description}</Td>
+                          <Td>
+                            <Button
+                              variant="solid"
+                              backgroundColor="#04212C"
+                              color="white"
+                              _hover={{
+                                backgroundColor: 'gray.500'
+                              }}
+                            >
+                              <Link href={`/g/${data.address}`}>
+                                View Group
+                              </Link>
+                            </Button>
+                          </Td>
+                          {/* <Td isNumeric>25.4</Td> */}
+                        </Tr>
+                      )
+                    })}
+                  </Tbody>
+                </Table>
+              </TableContainer>
+              <Flex flexDirection="column" />
             </Box>
           </Center>
         </Box>
@@ -140,65 +145,4 @@ export default function Groups(props: GetGroupProps) {
 export function getStaticProps() {
   const config = asConfig(process.env)
   return { props: { config } }
-}
-
-interface SingleGroupProps {
-  groupAddress: string
-  groupName: string
-  groupDescription: string
-  numMembers?: number
-}
-const SingleGroupItem = ({
-  groupAddress,
-  groupName,
-  groupDescription,
-  numMembers
-}: SingleGroupProps): JSX.Element => {
-  return (
-    //     {groupName = 'Test Group',
-    // groupDescription = 'This is a very interesting description',
-    // numMembers = 23}
-    <Card
-      // className="nopaque"
-      // color="white"
-      mt="5%"
-      mr="2.5%"
-      ml="2.5%"
-      backdropFilter="auto"
-      backdropBlur="8px"
-      border="1px solid rgba(255, 255, 255, 0.4)"
-      borderRadius="20px"
-      backgroundColor="rgba(255, 255, 255, 0.2)"
-      shadow="0px 0px 5px rgba(255,255,255,0.3), 1px 1px 5px rgba(255,255,255,0.3),10px 10px 30px rgba(0,0,0,0.3)"
-      direction={{ base: 'column', sm: 'row' }}
-      overflow="hidden"
-      variant="outline"
-    >
-      {/* <Image
-        objectFit="cover"
-        maxW={{ base: '100%', sm: '200px' }}
-        src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-        alt="Caffe Latte"
-      /> */}
-      <Stack>
-        <CardBody className="nopaque">
-          <Heading size="md">{groupName}</Heading>
-          <Text py="2">{groupDescription}</Text>
-          <Text py="2">{numMembers}</Text>
-        </CardBody>
-        <CardFooter>
-          <Button
-            variant="solid"
-            backgroundColor="#04212C"
-            color="white"
-            _hover={{
-              backgroundColor: 'gray.500'
-            }}
-          >
-            <Link href={`/g/${groupAddress}`}>View Group</Link>
-          </Button>
-        </CardFooter>
-      </Stack>
-    </Card>
-  )
 }
